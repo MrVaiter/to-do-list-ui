faf0d356105624a2c4fa1e87509a9eed7c0f1be2
mrvaiter <dima-25@ukr.net>
2024-05-09 20:19:27 +0200

    Fix build

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    path: require.resolve("path-browserify"),
+    os: require.resolve("os-browserify/browser"),
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    buffer: require.resolve("buffer/")
+  };
+  return config;
+};
